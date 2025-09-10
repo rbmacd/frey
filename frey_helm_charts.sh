1d8abb336188abf7e1eefdab63a43d8123a6deb4
rbmacd <robert.b.macdonald@gmail.com>
2025-09-10 13:53:15 -0500

    updated frey_helm_charts

--- frey_helm_charts.sh
+++ frey_helm_charts.sh
@@ -1,3 +1,8 @@
 #/bin/sh
-helm --kubeconfig /etc/rancher/k3s/k3s.yaml install --namespace awx --create-namespace awx-operator awx-operator/awx-operator -f services/awx/frey-awx-values.yaml
-helm --kubeconfig /etc/rancher/k3s/k3s.yaml install netbox oci://ghcr.io/netbox-community/netbox-chart/netbox --create-namespace --namespace netbox -f services/netbox/frey-netbox-values.yaml
+
+### k3s helm commands with kubeconfig manually defined
+#helm --kubeconfig /etc/rancher/k3s/k3s.yaml install --namespace awx --create-namespace awx-operator awx-operator/awx-operator -f services/awx/frey-awx-values.yaml
+#helm --kubeconfig /etc/rancher/k3s/k3s.yaml install netbox oci://ghcr.io/netbox-community/netbox-chart/netbox --create-namespace --namespace netbox -f services/netbox/frey-netbox-values.yaml
+
+helm install --namespace awx --create-namespace awx-operator awx-operator/awx-operator -f services/awx/frey-awx-values.yaml
+helm install netbox oci://ghcr.io/netbox-community/netbox-chart/netbox --create-namespace --namespace netbox -f services/netbox/frey-netbox-values.yaml
